6278e38a67186686b2df003bc0ef40749ab3d49d
CheolminConanShin <scm0226@gmail.com>
2017-03-20 08:54:56 +0900

    add jest test

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,11 +1,11 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import {App} from '../App/View/App.js';
 
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
     const tree = renderer.create(
-        <Index />
+        <App />
     );
 });
