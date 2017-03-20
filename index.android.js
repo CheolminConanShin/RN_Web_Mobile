bc4743cf8f403cd6d17f6a3df3e6033ec3efef33
Conan Shin and Kris Gonzalez <pair+conan+kris@samsung.com>
2017-03-20 13:56:04 +0900

    sources same VideoApp.js for react-native-video playback in iOS & Android

--- index.android.js
+++ index.android.js
@@ -1,4 +1,5 @@
 import {AppRegistry} from 'react-native';
-import {App} from './App/View/App';
 
-AppRegistry.registerComponent('cld', () => App);
+import {VideoPlayer} from './App/View/VideoApp';
+
+AppRegistry.registerComponent('cld', () => VideoPlayer);
